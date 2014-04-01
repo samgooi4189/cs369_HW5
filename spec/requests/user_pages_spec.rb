3a15eb8162d8a64f376dacc8be05a899a0ad09f5
Gooi <samgooi4189@gmail.com>
2014-04-01 17:16:28 -0500

    Add module for signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -37,13 +37,8 @@ describe "User pages" do
    end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",		with: "Example User"
-	fill_in "Email",	with: "user@example.com"
-	fill_in "Password", 	with: "foobar"
-	fill_in "Confirmation",	with: "foobar"
-      end
-
+      before { fill_signup }
+     
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,10 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
+
+def fill_signup
+  fill_in "Name",		with: "Example User"
+  fill_in "Email", 		with: "user@example.com"
+  fill_in "Password",		with: "foobar"
+  fill_in "Confirmation",	with: "foobar"
+end
