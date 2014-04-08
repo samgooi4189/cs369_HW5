eb6ad183c77ac25560365d181a9ace1f797e39c2
Gooi <samgooi4189@gmail.com>
2014-04-08 16:17:01 -0500

    Something wrong with user controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -7,8 +9,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
- 
-  def create
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+   def def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -22,5 +37,16 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # Before filters
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
