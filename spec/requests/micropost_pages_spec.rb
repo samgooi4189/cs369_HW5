71f8e7aac2c0a3d3df2944068bce3d6c5d729b01
Gooi <samgooi4189@gmail.com>
2014-04-17 18:29:44 -0500

    Add test to ensure user not able to delete other user's post

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -40,6 +40,14 @@ describe "Micropost pages" do
       it "should delete a micropost" do
         expect { click_link "delete" }.to change(Micropost, :count).by(-1)
       end
+
+      describe "looking into micropost that does not belong to him" do
+        let(:new_user) { FactoryGirl.create(:user) } 
+        before { visit user_path(new_user)}
+        it "should not able to see delete link" do
+          expect(page).not_to have_link('delete')
+        end
+      end
     end
   end
 end
