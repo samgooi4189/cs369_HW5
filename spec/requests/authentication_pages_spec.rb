cdd8eedb5be0610911bafb59a98523b0f2034f2d
Gooi <samgooi4189@gmail.com>
2014-04-17 00:18:19 -0500

    finished until 10.34

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -108,6 +108,18 @@ describe "Authentication" do
         			end
       			end
 
+			describe "in the Microposts controller" do
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { expect(response).to redirect_to(signin_path) }
+ 				end
+
+				describe "submitting to the destory action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+
 			it { should_not have_link('Profile', href: user )}
 			it { should_not have_link('Settings', href: edit_user_path(user) )}
     		end
